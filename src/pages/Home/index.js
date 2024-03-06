604bba3c966811713968d3473752fd6f71c1352f
gluisjulian <gluisjulian@gmail.com>
2024-03-06 19:59:56 -0300

    Carregando filmes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,6 +7,7 @@ import './Home.css'
 function Home(){
 
     const [filmes, setFilmes] = useState([]);
+    const [loading, setLoading] = useState(true);
 
 
     useEffect(() => {
@@ -21,10 +22,20 @@ function Home(){
 
             //console.log(response.data.results);
             setFilmes(response.data.results.slice(0, 10));
+            setLoading(false);
         }
 
         loadFilmes();
     }, []);
+
+
+    if(loading){
+        return(
+            <div className="loading">
+                <h2>Carregando filmes...</h2>
+            </div>
+        )
+    }
 
 
     return(
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Filme from './pages/Filme';
 import Header from "./components/Header";
+import Erro from './pages/Erro';
 
 function RoutesApp()
 {
@@ -11,8 +12,11 @@ function RoutesApp()
         <BrowserRouter>
             <Header/>
                 <Routes>
-                    <Route path="/" element={ <Home/> }/>
-                    <Route path="/filme/:id" element={ <Filme/> }/>
+                    <Route path="/" element={ <Home /> }/>
+                    <Route path="/filme/:id" element={ <Filme /> }/>
+
+
+                    <Route path="*" element={<Erro />} />
                 </Routes>
         </BrowserRouter>
     )
